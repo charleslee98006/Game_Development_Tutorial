a7149b56d4de93c7eea77bb07dc218c3ffa9be00
charles <charles.98006@gmail.com>
2017-09-19 23:37:21 -0400

    Adding movement and camera fixing to the player.

---
+++ Udemy-2DPlatformer/2D Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject target;
+	public float followAhead;
+	public float smoothing;
+
+	private Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// this moves the target of the camera ahead of the player in both left and right direction.
+		targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+		if (target.transform.localScale.x > 0f) {
+			targetPosition = new Vector3 (targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+		} else {
+			targetPosition = new Vector3 (targetPosition.x -followAhead, targetPosition.y, targetPosition.z);
+
+		}
+		transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing*Time.deltaTime);
+
+		//transform.position = targetPosition;
+	}
+}
---
+++ Udemy-2DPlatformer/2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float moveSpeed;
+	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
+	public float jumpSpeed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	public bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+		if (Input.GetAxisRaw ("Horizontal") > 0f) {
+			myRigidBody.velocity = new Vector3 (moveSpeed, myRigidBody.velocity.y, 0f);
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			myRigidBody.velocity = new Vector3 (-moveSpeed, myRigidBody.velocity.y, 0f);
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+		} else {
+			myRigidBody.velocity = new Vector3 (0f, myRigidBody.velocity.y, 0f);
+
+		}
+		if(Input.GetButtonDown("Jump")  && isGrounded){
+			myRigidBody.velocity = new Vector3 (myRigidBody.velocity.x, jumpSpeed, 0f);
+		}
+		myAnim.SetFloat ("Speed", Mathf.Abs(myRigidBody.velocity.x));
+		myAnim.SetBool ("Grounded", isGrounded);
+	}
+}
